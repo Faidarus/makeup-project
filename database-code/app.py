82770b48684000c241d7b723d167d08a325df04d
Faidarus <fatima_aidarus@hotmail.co.uk>
2021-03-11 09:23:17 +0000

    made changes to app.py

--- database-code/app.py
+++ database-code/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root@35.189.66.154/my_make_up_bag"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root@35.189.66.154/mymakeupbag_data"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=True
 db = SQLAlchemy(app)
 
